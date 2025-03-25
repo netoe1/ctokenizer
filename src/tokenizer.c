b89185efe9634d709541b41fc71d3203254b76ce
netoe1 <elynetobr@outlook.com>
2025-03-24 21:34:32 -0300

    refactoring

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -5,97 +5,49 @@
 #include <stdbool.h>
 #include <string.h>
 
-struct Tokens
+struct TokenUnitary
 {
     int size;
-    char **vector;
+    char *str;
 };
 
-typedef struct Tokens Tokens;
+struct TokenList
+{
+    int size;
+    struct TokenUnitary *tokens;
+};
+
+typedef struct TokenList TokenList;
+typedef struct TokenUnitary TokenUnitary;
 
 static void verify_ptr_and_exit(void *ptr, const char *err_message);
 static void verify_str_and_exit(const char *ptr);
-
-void init_tokens(Tokens *ptr);
-void free_vectors(Tokens *ptr);
-void tokenize(Tokens *ptr, const char *prhase, const char *lead_to_cut);
-void debug_get_tokens(Tokens *ptr);
-
-static void verify_str_and_exit(const char *ptr)
-{
-    if (ptr == NULL || ptr[0] == '\0')
-    {
-        fprintf(stderr, "[verify_str-err]: ptr must be a valid pointer!\n");
-        exit(EXIT_FAILURE);
-    }
-}
+void initialize(TokenList *list);
 
 static void verify_ptr_and_exit(void *ptr, const char *err_message)
 {
-    if (ptr == NULL)
+    if (!ptr)
     {
-        fprintf(stderr, "%s\n", err_message);
+        perror("[vefptr-err]: Invalid Pointer");
         exit(EXIT_FAILURE);
     }
+    perror(err_message);
+    exit(EXIT_FAILURE);
 }
-
-void init_tokens(Tokens *ptr) // Stable
+static void verify_str_and_exit(const char *ptr)
 {
-    ptr->vector = NULL;
-    ptr->size = 0;
-    puts("[init_tokens-debug]: Tokens init with success.");
-}
-
-void free_vectors(Tokens *ptr) // Stable
-{
-    if (ptr == NULL)
-
-        return;
-
-    for (int i = 0; i < ptr->size; i++)
+    if (!ptr || ptr[0] == '\0')
     {
-
-        printf("\n[free_vectors-debug]: Try to free pointer at %d position...\n", i);
-        free(ptr->vector[i]);
-    }
-
-    free(ptr->vector);
-}
-
-void tokenize(Tokens *ptr, const char phrase[], const char *lead_to_find)
-{
-    verify_ptr_and_exit((void *)ptr, "[tokenize-err]: invalid ptr structure");
-    verify_str_and_exit(phrase);
-    verify_str_and_exit(lead_to_find);
-
-    char *phraseMutable = strdup(phrase);
-    char *token = strtok(phraseMutable, lead_to_find);
-    if (token == NULL)
-    {
-        fprintf(stderr, "[tokenize-err]: buffer not capable to tokenize.");
-        free_vectors(ptr);
-        free(phraseMutable);
-        exit(EXIT_FAILURE);
-    }
-
-    while (token != NULL)
-    {
-        phraseMutable = strtok(NULL, lead_to_find);
-
-        if (ptr->size == 0)
-        {
-        }
-        ptr->size++;
-    }
-    free(phraseMutable);
-}
-
-void debug_get_tokens(Tokens *ptr)
-{
-    verify_ptr_and_exit(ptr, "[debug_get_tokens-err]: invalid Tokens *ptr at function parameter.");
-    for (int i = 0; i == ptr->size; i++)
-    {
-        printf("\nToken [%d]:%s", i, ptr->vector[i]);
+        verify_ptr_and_exit((void *)ptr, "[vefstr-err]: Invalid String.");
     }
 }
+
+void initialize(TokenList *list)
+{
+    verify_ptr_and_exit((void *)list, "[initialize-err]: invalid ptr to TokenList");
+
+    list->size = 0;
+    list->tokens = NULL;
+}
+
 #endif
